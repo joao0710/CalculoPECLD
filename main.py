74d7bfd52593fcb5cca0297e104f18145df99cc3
joao0710 <joao0710@gmail.com>
2025-08-15 12:20:55 -0300

    Scaffolding

---
+++ main.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+janela = Tk()
+janela.title("Calculadora PECLD")
+janela.state('zoomed')
+janela.configure(bg="black")
+janela.iconbitmap('images/botao.ico')
+
+janela.mainloop()
