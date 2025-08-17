168a1c947c702c2a9037521dfd0ac9d747d12561
joao0710 <joao0710@gmail.com>
2025-08-17 12:55:01 -0300

    Configurada variável DEBUG

--- Calculadora/settings.py
+++ Calculadora/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-naj6u(@3izi*=t#@!(a2943$@nk&r&pfp+3guj8thsan4i!3$5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
