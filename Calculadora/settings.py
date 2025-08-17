000e69d07466d56a77ca88b3aa439e5418b78eb0
joao0710 <joao0710@gmail.com>
2025-08-17 13:16:02 -0300

    Configuração SECRET KEY

--- Calculadora/settings.py
+++ Calculadora/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-naj6u(@3izi*=t#@!(a2943$@nk&r&pfp+3guj8thsan4i!3$5'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
